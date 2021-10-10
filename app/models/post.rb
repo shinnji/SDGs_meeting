f45582cd00a96a0961c067870c2d38c9beb86982
shinnji <shinnji02080602@gmail.com>
2021-10-10 14:53:57 +0000

    Post function_create

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+  attachment :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :body
+    validates :image
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a8a9695a9a8d10e7df08cd48458b416a0bd291b59364c5797b877bec5583fd17882ee89a5c66b1f88abf615105f599b23f8149a2d34ced3a9fc1b43be49d4ada'
