662c0c68c3ff828540160a78bb4e01a90ab9109a
shinnji <shinnji02080602@gmail.com>
2021-10-28 05:04:04 +0000

    Error resolution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    
     @user=User.find(params[:id])
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
